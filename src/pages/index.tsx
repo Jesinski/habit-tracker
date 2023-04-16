13eb87c45a059479093fcd52250b2741aa2d1373
Gustavo <gustavojesinskib@gmail.com>
2023-04-16 09:56:22 -0300

    layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import Tile from "@/components/Tile";
 import { Inter } from "next/font/google";
-
+import { BiHome, BiTachometer } from "react-icons/bi";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="p-2">
-      <h1 className="font-extrabold uppercase text-3xl">Routine Center</h1>
-      <div className="flex flex-col">
+    <div className="flex flex-col h-screen">
+      <header className="p-2 m-2 h-10 font-extrabold uppercase text-3xl">
+        Routine Center
+      </header>
+      <main className="overflow-auto">
         <Tile time="5:00 AM" name="Wake Up"></Tile>
         <Tile time="6:00 AM" name="Gym"></Tile>
         <Tile time="7:00 AM" name="1st Meal"></Tile>
@@ -18,7 +20,17 @@ export default function Home() {
         <Tile time="7:15 PM" name="5th Meal"></Tile>
         <Tile time="7:00 PM" name="1st Meal"></Tile>
         <Tile time="9:00 PM" name="Sleep"></Tile>
-      </div>
+      </main>
+      <footer className="flex justify-around h-24 max-h-24 w-full bg-gray-200">
+        <div className="flex flex-col justify-center">
+          <BiHome size={24} className="self-center flexgrow" />
+          <button className="bg-transparent text-xs">Home</button>
+        </div>
+        <div className="flex flex-col justify-center">
+          <BiTachometer size={24} className="self-center flexgrow" />
+          <button className="bg-transparent text-xs">Progress</button>
+        </div>
+      </footer>
     </div>
   );
 }
