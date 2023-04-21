01fb933564a8a0eefb0cf3dab8096dac70aaab7f
Gustavo <gustavojesinskib@gmail.com>
2023-04-21 13:38:04 -0300

    color on swipe!

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -5,19 +5,36 @@ export default function Tile(props: { name: string; time: string }) {
   const [left, setLeft] = useState(0);
   const [currentLeft, setCurrentLeft] = useState(0);
   const [startX, setStartX] = useState(0);
+
+  const [transition, setTransition] = useState({
+    transition: "none",
+  });
+
+  const [bgColor, setBgColor] = useState({
+    backgroundColor: "none",
+  });
   const handlers = useSwipeable({
     onSwipedLeft: (eventData) => {
       console.log("Failed task!", eventData.event.target);
+      setBgColor({ backgroundColor: "red" });
     },
     onSwipedRight: (eventData) => {
       console.log("Completed task!", eventData.event.target);
+      setBgColor({ backgroundColor: "green" });
     },
   });
 
   const onPointerDown = (e: PointerEvent) => {
     e.preventDefault();
     setStartX(e.pageX);
+    setTransition({ transition: "" });
     setCurrentLeft(left);
+  };
+
+  const onPointerUp = (e: PointerEvent) => {
+    e.preventDefault();
+    setTransition({ transition: "left 0.5s" });
+    setLeft(0);
   };
 
   const move = (e: PointerEvent) => {
@@ -27,20 +44,19 @@ export default function Tile(props: { name: string; time: string }) {
   };
 
   return (
-    <div
-      style={{ left: left }}
-      className={`relative touch-none`}
-      onPointerDown={onPointerDown}
-      onPointerMove={move}
-      {...handlers}
-    >
-      <section className={`p-2 mx-2 my-2`}>
-        <span className="my-1 text-xl"> {props.time} </span>
-        <div className="flex justify-between bg-blue-200 rounded-xl">
-          <span className="p-1 m-2 text-2xl">{props.name}</span>
-          <span className="p-1 m-2 text-lg">Category</span>
-        </div>
-      </section>
-    </div>
+    <section className={`p-2 mx-2 my-2`}>
+      <span className="my-1 text-xl"> {props.time} </span>
+      <div
+        style={{ left: left, ...transition, ...bgColor }}
+        onPointerDown={onPointerDown}
+        onPointerMove={move}
+        onPointerUp={onPointerUp}
+        {...handlers}
+        className="flex justify-between relative touch-none bg-blue-200 rounded-xl"
+      >
+        <span className="p-1 m-2 text-2xl">{props.name}</span>
+        <span className="p-1 m-2 text-lg">Category</span>
+      </div>
+    </section>
   );
 }
