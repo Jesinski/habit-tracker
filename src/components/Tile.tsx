f24cd6513a57a1bb33290c63b723273405a53d1c
Gustavo <gustavojesinskib@gmail.com>
2023-04-21 10:03:59 -0300

    porra finalmente

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,24 +1,39 @@
-import { useState } from "react";
+import { PointerEvent, useState } from "react";
 import { useSwipeable } from "react-swipeable";
 
 export default function Tile(props: { name: string; time: string }) {
   const [left, setLeft] = useState(0);
-
+  const [currentLeft, setCurrentLeft] = useState(0);
+  const [startX, setStartX] = useState(0);
   const handlers = useSwipeable({
     onSwipedLeft: (eventData) => {
-      const delta = eventData.deltaX;
-      setLeft(delta);
       console.log("Failed task!", eventData.event.target);
     },
     onSwipedRight: (eventData) => {
-      const delta = eventData.deltaX;
-      setLeft(delta);
       console.log("Completed task!", eventData.event.target);
     },
   });
 
+  const onPointerDown = (e: PointerEvent) => {
+    e.preventDefault();
+    setStartX(e.pageX);
+    setCurrentLeft(left);
+  };
+
+  const move = (e: PointerEvent) => {
+    e.preventDefault();
+    const currentX = e.pageX;
+    setLeft(currentLeft + currentX - startX);
+  };
+
   return (
-    <div style={{ left: left }} className={`relative`} {...handlers}>
+    <div
+      style={{ left: left }}
+      className={`relative touch-none`}
+      onPointerDown={onPointerDown}
+      onPointerMove={move}
+      {...handlers}
+    >
       <section className={`p-2 mx-2 my-2`}>
         <span className="my-1 text-xl"> {props.time} </span>
         <div className="flex justify-between bg-blue-200 rounded-xl">
