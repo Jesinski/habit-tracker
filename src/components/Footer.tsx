3ab3c789d062d1775b3adbc79c8bd5e7d86ba1e7
Gustavo <gustavojesinskib@gmail.com>
2023-04-21 14:30:38 -0300

    mistakes were made

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { BiHome, BiTachometer } from "react-icons/bi";
 
 export default function Footer() {
   return (
-    <footer className="flex-none bottom-0 flex justify-around h-14 max-h-14 w-full bg-gray-200 bg-red-800">
+    <footer className="flex-none bottom-0 flex justify-around h-14 max-h-14 w-full bg-gray-200">
       <Link href="/" className="flex flex-col justify-center">
         <BiHome size={24} className="self-center flexgrow" />
         <button className="bg-transparent text-xs">Home</button>
