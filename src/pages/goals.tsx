2fad0c4dcbdec388457b2d550d614144c00a0d9b
Gustavo <gustavojesinskib@gmail.com>
2023-04-22 13:49:25 -0300

    Goals Page

--- src/pages/goals.tsx
+++ src/pages/goals.tsx
@@ -7,7 +7,51 @@ export default function Goals() {
     <Layout>
       <Header title="Goals" />
       <Content>
-        <span></span>
+        <div className="flex flex-col p-2 space-y-4">
+          <div className="flex items-center justify-between border-b-2 border-gray-400 bg-green-200 rounded-md h-12 px-2">
+            <span className=" text-2xl font-semibold">Sleep</span>
+            <span className=" text-2xl font-semibold">80%</span>
+          </div>
+          <div className="flex items-center justify-between border-b-2 border-gray-400 bg-green-400 rounded-md h-12 px-2">
+            <span className=" text-2xl font-semibold">Workout</span>
+            <span className=" text-2xl font-semibold">60%</span>
+          </div>
+          <div className="flex items-center justify-between border-b-2 border-gray-400 bg-green-100 rounded-md h-12 px-2">
+            <span className=" text-2xl font-semibold">Nutrition</span>
+            <span className=" text-2xl font-semibold">90%</span>
+          </div>
+          <div className="flex items-center justify-between border-b-2 border-gray-400 bg-green-600 rounded-md h-12 px-2">
+            <span className=" text-2xl font-semibold">Water</span>
+            <span className=" text-2xl font-semibold">95%</span>
+          </div>
+          <div className="flex items-center justify-between border-b-2 border-gray-400 bg-green-50 rounded-md h-12 px-2">
+            <span className=" text-2xl font-semibold">Overall</span>
+            <span className=" text-2xl font-semibold">80%</span>
+          </div>
+        </div>
+        <div className="border-b-2 border-black my-4" />
+
+        <div className="flex flex-col space-y-2">
+          <span className="font-bold uppercase"> How we set your goals</span>
+          <span>
+            We believe that progress are not linear. Life is somewhat
+            unexpected, therefore we need to take this into account to decide
+            the goal.
+          </span>
+          <span>
+            We collect data points based on the individual goals to determine
+            future targets. Crossing data such series length, usage adherence,
+            subjective perception of difficultness, etc.
+          </span>
+          <span>
+            If you achieve your Water Goal with ease, on the next phase it will
+            be higher.
+          </span>
+          <span>
+            On the other way around, if you did not achieve a target, it will be
+            reduced accordingly, in order to not burn you out.
+          </span>
+        </div>
       </Content>
     </Layout>
   );
