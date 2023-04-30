66c42e7240be05838016a2b209cfd4aac4e4c0d3
Gustavo Jesinski <gustavojesinskib@gmail.com>
2023-04-30 16:13:26 -0300

    updated prisma?

--- src/pages/api/putTask.ts
+++ src/pages/api/putTask.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { tasks } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
-import prisma from "../../lib/prisma";
+import { prisma } from "./db";
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<tasks>
