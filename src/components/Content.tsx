ff5130a02ba249fa7eca7aea3c6a343fc91a23c7
Gustavo Jesinski <gustavojesinskib@gmail.com>
2023-05-13 15:57:04 -0300

    lol

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -4,10 +4,16 @@ export default function Content(props: { children: ReactNode }) {
   const ref = useRef<HTMLDivElement>(null);
 
   useLayoutEffect(() => {
-    ref.current?.scrollTo({
-      top: ref.current?.scrollHeight / 3,
-      behavior: "smooth",
-    });
+    // TODO: Refactor this behavior
+    // This is a one-of-a-kind gambiarra.
+    // If user is on Home page
+    if (ref.current?.baseURI.endsWith("/")) {
+      // Scroll to the middle, where the next tasks to be completed should be
+      ref.current?.scrollTo({
+        top: ref.current?.scrollHeight / 3,
+        behavior: "smooth",
+      });
+    }
   });
 
   return (
