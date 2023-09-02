11447fa21752bafa377b3ce52436a9b39ebecadb
Gustavo Jesinski <gustavojesinskib@gmail.com>
2023-09-02 18:16:28 -0300

    cleaning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,121 +6,6 @@ import { Database } from "@/types/database-generated.types";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
-const DATA = [
-  {
-    id: 1,
-    name: "Chest Day",
-    category: "Workout",
-    datetime: new Date(),
-    completed: 1,
-    projectid: 2,
-  },
-  {
-    id: 1,
-    name: "Chest Day",
-    category: "Workout",
-    datetime: new Date(),
-    completed: 1,
-    projectid: 2,
-  },
-  {
-    id: 1,
-    name: "Chest Day",
-    category: "Workout",
-    datetime: new Date(),
-    completed: 1,
-    projectid: 2,
-  },
-  {
-    id: 1,
-    name: "Chest Day",
-    category: "Workout",
-    datetime: new Date(),
-    completed: 1,
-    projectid: 2,
-  },
-  {
-    id: 1,
-    name: "Chest Day",
-    category: "Workout",
-    datetime: new Date(),
-    completed: 1,
-    projectid: 2,
-  },
-  {
-    id: 1,
-    name: "Chest Day",
-    category: "Workout",
-    datetime: new Date(),
-    completed: 1,
-    projectid: 2,
-  },
-  {
-    id: 1,
-    name: "Chest Day",
-    category: "Workout",
-    datetime: new Date(),
-    completed: 1,
-    projectid: 2,
-  },
-  {
-    id: 1,
-    name: "Chest Day",
-    category: "Workout",
-    datetime: new Date(),
-    completed: 1,
-    projectid: 2,
-  },
-  {
-    id: 1,
-    name: "Chest Day",
-    category: "Workout",
-    datetime: new Date(),
-    completed: 1,
-    projectid: 2,
-  },
-  {
-    id: 1,
-    name: "Chest Day",
-    category: "Workout",
-    datetime: new Date(),
-    completed: 1,
-    projectid: 2,
-  },
-  {
-    id: 1,
-    name: "Chest Day",
-    category: "Workout",
-    datetime: new Date(),
-    completed: 1,
-    projectid: 2,
-  },
-  {
-    id: 1,
-    name: "Chest Day",
-    category: "Workout",
-    datetime: new Date(),
-    completed: 1,
-    projectid: 2,
-  },
-  {
-    id: 1,
-    name: "Chest Day",
-    category: "Workout",
-    datetime: new Date(),
-    completed: 1,
-    projectid: 2,
-  },
-  {
-    id: 1,
-    name: "Chest Day",
-    category: "Workout",
-    datetime: new Date(),
-    completed: 1,
-    projectid: 2,
-  },
-];
-
 export default async function Page() {
   const supabase = createServerComponentClient<Database>({ cookies });
   const tomorrow = new Date();
@@ -137,7 +22,7 @@ export default async function Page() {
       new Date(yesterday.setDate(yesterday.getDate() - 1)).toISOString()
     )
     .order("time");
-  console.log(data);
+
   return (
     <>
       <Header title="Routine Center" />
