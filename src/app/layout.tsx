8ede2f80a731be46b79dba51fe920f653b2f4d3f
Gustavo Jesinski <gustavojesinskib@gmail.com>
2023-09-02 10:19:20 -0300

    fixed some metadata stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,34 @@
 import Footer from "@/components/Footer";
 import "@/styles/globals.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Health Tracker",
+  description: "test description",
+  twitter: {
+    card: "summary_large_image",
+    creator: "@imamdev_",
+    images: "https://example.com/og.png",
+  },
+  applicationName: "Health Tracker",
+  appleWebApp: {
+    capable: true,
+    title: "Health Tracker",
+    statusBarStyle: "default",
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  themeColor: "#FFFFFF",
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  manifest: "/manifest.json",
+  icons: [
+    { rel: "apple-touch-icon", url: "/icons/apple-touch-icon.png" },
+    { rel: "shortcut icon", url: "/favicon.ico" },
+  ],
+  keywords: ["nextjs", "pwa", "next-pwa"],
+};
 
 export default function RootLayout({
   // Layouts must accept a children prop.
@@ -10,7 +39,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="flex flex-col h-screen">
+      <body className="flex flex-col h-screen ">
         {children}
         <Footer />
       </body>
