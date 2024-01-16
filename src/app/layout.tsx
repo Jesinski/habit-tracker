7079893e1154d54dc2d552b1ff97a245265e5a5c
Gustavo Jesinski <gustavojesinskib@gmail.com>
2024-01-16 16:10:25 -0300

    fixed layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
 import "@/styles/globals.css";
-import { Metadata } from "next";
+import { Metadata, Viewport } from "next";
 
 export const metadata: Metadata = {
   title: "Health Tracker",
   description: "test description",
-  twitter: {
-    card: "summary_large_image",
-    creator: "@imamdev_",
-    images: "https://example.com/og.png",
-  },
+  // twitter: {
+  //   card: "summary_large_image",
+  //   creator: "@imamdev_",
+  //   images: "https://example.com/og.png",
+  // },
   applicationName: "Health Tracker",
   appleWebApp: {
     capable: true,
@@ -18,15 +18,21 @@ export const metadata: Metadata = {
   formatDetection: {
     telephone: false,
   },
-  themeColor: "#FFFFFF",
-  viewport:
-    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+
   manifest: "/manifest.json",
   icons: [
     { rel: "apple-touch-icon", url: "/icons/apple-touch-icon.png" },
     { rel: "shortcut icon", url: "/favicon.ico" },
   ],
   keywords: ["nextjs", "pwa", "next-pwa"],
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
+  width: "device-width",
+  initialScale: 1,
+  minimumScale: 1,
+  viewportFit: "cover",
 };
 
 export default function RootLayout({
@@ -38,7 +44,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="flex flex-col h-screen">{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
